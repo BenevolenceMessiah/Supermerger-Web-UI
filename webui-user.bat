19d229d31ddcfef9c71d3d4c008602fb34d047d7
Benevolence Messiah <120072200+BenevolenceMessiah@users.noreply.github.com>
2024-08-19 21:15:55 -0400

    reconfigure Gradio.

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON= py -3.10
 set GIT=
 set VENV_DIR= .venv
-set COMMANDLINE_ARGS= --xformers --no-half --no-half-vae --api --opt-split-attention --precision full --port 7861 --autolaunch --ui-settings-file supermerger-config.json
+set COMMANDLINE_ARGS= --xformers --no-half --no-half-vae --api --opt-split-attention --precision full --port 7862 --autolaunch --ui-settings-file supermerger-config.json
 
 call webui.bat
